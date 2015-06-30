83234f10a92b1540a339759e73accd09becf0af4
Elliot Block <elliot@framed.io>
2015-06-30 18:40:13 +0000

    Upgrade to Carmine 2.11.1
    This fixes an incompatibility issue with Clojure 1.7

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.taoensso/carmine "2.9.0"]
+                 [com.taoensso/carmine "2.11.1"]
                  [org.clojure/test.check "0.7.0"]]
   :profiles {:uberjar {:aot :all}}
   :plugins [[codox "0.8.12"]]
